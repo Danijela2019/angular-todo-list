780d0571bd8e0b47241c9b0f4b977974cef5dc78
Danijela2019 <arsic_danijela@yahoo.com>
2021-03-09 13:51:46 +0100

    Restructuring files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name :string = 'Danijela';
+  name :string = 'User';
 
 
   constructor(){
